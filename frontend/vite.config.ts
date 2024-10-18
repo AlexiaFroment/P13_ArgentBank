17e27d2294417ff4b4a78a29561f094f9a9acb1a
AloaCmoi <aloacmoi@gmail.com>
2024-10-18 18:03:37 +0200

    Implementation file frontend with hot reloading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+
   server: {
-    host: "0.0.0.0", // Ajoute cette ligne
-    port: 5173, // Assure-toi que le port est correct
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
   },
 })
